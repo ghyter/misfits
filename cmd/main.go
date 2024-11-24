849c0eb88edbb03251eff58528cd297a120d76a7
Gabriel Hyter <ghyter@gmail.com>
2024-11-24 18:22:05 +0000

    ebiten game baseline

---
+++ cmd/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"golang.org/x/image/font/basicfont"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Clear the screen with a background color
+	screen.Fill(color.RGBA{R: 50, G: 50, B: 150, A: 255})
+
+	// Draw a simple rectangle
+	rect := ebiten.NewImage(100, 50)
+	rect.Fill(color.RGBA{R: 255, G: 0, B: 0, A: 255})
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(350, 275)
+	screen.DrawImage(rect, op)
+
+	// Display "Hello, Ebiten!" text
+	text.Draw(screen, "Hello, Ebiten!", basicfont.Face7x13, 350, 250, color.White)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 800, 600
+}
+
+func main() {
+	game := &Game{}
+	ebiten.RunGame(game)
+}
